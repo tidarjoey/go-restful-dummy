8d5b8d592c6c1590adb73a6bfcbbd16a01301e78
Tidar <tidarharyo@rocketmail.com>
2019-01-17 15:16:06 +0700

    update main.go

--- main.go
+++ main.go
@@ -1,14 +1,9 @@
-// This is the name of our package
-// Everything with this package name can see everything
-// else inside the same package, regardless of the file they are in
 package main
 
-// These are the libraries we are going to use
-// Both "fmt" and "net" are part of the Go standard library
 import (
-	// "fmt" has methods for formatted I/O operations (like printing to the console)
+	// basic I/O methods
 	"fmt"
-	// The "net/http" library has methods to implement HTTP clients and servers
+	// implement HTTP clients and servers
 	"net/http"
   // gorilla/mux library as request routinng library
   "github.com/gorilla/mux"
@@ -19,19 +14,15 @@ func main() {
   r := mux.NewRouter()
 
 	// The "HandleFunc" method accepts a path and a function as arguments
-	// (Yes, we can pass functions as arguments, and even trat them like variables in Go)
-	// However, the handler function has to have the appropriate signature (as described by the "handler" function below)
 	r.HandleFunc("/hello", handler).Methods("GET")
 
-	// After defining our server, we finally "listen and serve" on port 8080
-	// The second argument is the handler, which we will come to later on, but for now it is left as nil,
-	// and the handler defined above (in "HandleFunc") is used
+	// "ListenAndServe" weill start http server that accepts address and handler as arguments.
 	http.ListenAndServe(":8080", r)
 }
 
-// "handler" is our handler function. It has to follow the function signature of a ResponseWriter and Request type
-// as the arguments.
+// "handler" is our handler function. It has to follow the function signature of a ResponseWriter and Request type as the arguments.
+// ResponseWriter is an interface that a handler use to create http response
 func handler(w http.ResponseWriter, r *http.Request) {
-	// For this case, we will always pipe "Hello World" into the response writer
+	// passinng value to the ResponseWriter
 	fmt.Fprintf(w, "it works, yay!")
 }
