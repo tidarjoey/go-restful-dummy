325e4e65ed341b3d65f4ac72aee71d895cb50ca6
Tidar <tidarharyo@rocketmail.com>
2019-01-19 19:12:59 +0700

    new routing and handler function.

--- main.go
+++ main.go
@@ -9,20 +9,38 @@ import (
   "github.com/gorilla/mux"
 )
 
-func main() {
-  //declare new router
+// routing function outside main function
+func newRouter() *mux.Router {
   r := mux.NewRouter()
-
-	// The "HandleFunc" method accepts a path and a function as arguments
-	r.HandleFunc("/hello", handler).Methods("GET")
-
-	// "ListenAndServe" weill start http server that accepts address and handler as arguments.
-	http.ListenAndServe(":8080", r)
+  r.HandleFunc("/testing-GET", handler).Methods("GET")
+  return r
 }
+
+// main function
+func main()  {
+  r := newRouter()
+  http.ListenAndServe(":8080", r)
+}
+
+// handler funtion
+func handler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "GET routinng works, yay!")
+}
+
+// func main() {
+//   //declare new router
+//   r := mux.NewRouter()
+//
+// 	// The "HandleFunc" method accepts a path and a function as arguments
+// 	r.HandleFunc("/hello", handler).Methods("GET")
+//
+// 	// "ListenAndServe" weill start http server that accepts address and handler as arguments.
+// 	http.ListenAndServe(":8080", r)
+// }
 
 // "handler" is our handler function. It has to follow the function signature of a ResponseWriter and Request type as the arguments.
 // ResponseWriter is an interface that a handler use to create http response
-func handler(w http.ResponseWriter, r *http.Request) {
-	// passinng value to the ResponseWriter
-	fmt.Fprintf(w, "it works, yay!")
-}
+// func handler(w http.ResponseWriter, r *http.Request) {
+// 	// passinng value to the ResponseWriter
+// 	fmt.Fprintf(w, "it works, yay!")
+// }
