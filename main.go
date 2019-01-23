aaa105fc7fd37b7aad3657db61c51844cf84e283
Tidar <tidarharyo@rocketmail.com>
2019-01-23 22:44:48 +0700

    cleanup main.go

--- main.go
+++ main.go
@@ -20,8 +20,13 @@ func newRouter() *mux.Router {
 	// declare static dir
 	staticFileDir := http.Dir("./assets")
 
-	// declare file server handler, with StripPrefix
+	// declare file server handler, with StripPrefix that will strip '/assets/' from request
+	// this is needed because we are serving the static files in a subdir called '/assets/'
+	// first argument is the prefix that will be stripped, second is the file server dir
 	staticFileHandler := http.StripPrefix("/assets/", http.FileServer(staticFileDir))
+
+	// we'll use PathPrefix method to math the routinngs from '/assets/'
+	r.PathPrefix("/assets/").Handler(staticFileHandler).Methods("GET")
 
   return r
 }
@@ -29,13 +34,15 @@ func newRouter() *mux.Router {
 // main function
 func main()  {
   r := newRouter()
-  // "ListenAndServe" weill start http server that accepts address and handler as arguments.
+
+	// "ListenAndServe" weill start http server that accepts address and handler as arguments.
   http.ListenAndServe(":8080", r)
 }
 
 // "handler" is our handler function. It has to follow the function signature of a ResponseWriter and Request type as the arguments.
 // ResponseWriter is an interface that a handler use to create http response
 func handler(w http.ResponseWriter, r *http.Request) {
+
 	// passinng value to the ResponseWriter
   fmt.Fprintf(w, "GET routinng works, yay!")
 }
